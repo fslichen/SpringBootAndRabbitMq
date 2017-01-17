0453e553797ee7e7c7aa50804029df1e6852da53
user <user@use1r-PC>
2017-01-17 17:36:16 +0800

    There is no need to close the connection. Otherwise the main program stops. However, the main procedure is not supposed to stop.

--- SpringBootAndRabbitMq/src/main/java/hello/Sender.java
+++ SpringBootAndRabbitMq/src/main/java/hello/Sender.java
@@ -3,21 +3,16 @@ package hello;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
-import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Sender implements CommandLineRunner {
 	@Autowired
     private RabbitTemplate rabbitTemplate;
-	
-	@Autowired
-    private ConfigurableApplicationContext context;
 
     @Override
     public void run(String... args) throws Exception {
         System.out.println("Ready to send a message.");
         rabbitTemplate.convertAndSend(Application.queueName, "Hello World");
-        context.close();
     }
 }
